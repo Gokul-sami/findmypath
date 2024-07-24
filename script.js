7257d496127143d3311e240f5466a27b2f6da3b0
gokul <gokulpandiyan6@gmail.com>
2024-07-24 19:12:13 +0530

    new changes

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase
 import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAPAt8yoHglBADaGEhWGM1kZVgOzWhw8rE",
-    authDomain: "findyourpath-bcf56.firebaseapp.com",
-    projectId: "findyourpath-bcf56",
-    storageBucket: "findyourpath-bcf56.appspot.com",
-    messagingSenderId: "205317328979",
-    appId: "1:205317328979:web:d49c38e66dc9821a4276a6",
-    measurementId: "G-2YW6EBNFN2"
-  };
+  apiKey: "AIzaSyAPAt8yoHglBADaGEhWGM1kZVgOzWhw8rE",
+  authDomain: "findyourpath-bcf56.firebaseapp.com",
+  projectId: "findyourpath-bcf56",
+  storageBucket: "findyourpath-bcf56.appspot.com",
+  messagingSenderId: "205317328979",
+  appId: "1:205317328979:web:d49c38e66dc9821a4276a6",
+  measurementId: "G-2YW6EBNFN2"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
